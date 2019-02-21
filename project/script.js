ddc62edf0dd07c73e05e79fb6c22989026a3f184
Holreis <shayazdanov.artem@gmail.com>
2019-02-21 16:55:28 +0300

    hz

---
+++ project/script.js
@@ -0,0 +1,45 @@
+/*@media screen and ( max-width: 420px){
+	.header-container{
+		width: 100%;
+		display: flex;
+		justify-content: space-around;
+	}
+	.main-menu {
+		display:none;
+	}
+	.main-meny li{
+		width: 100%;
+		display: block;
+		font-size : 12px;
+	}
+	.main-menu a{
+		display: block;
+		padding: 8px;
+
+	}
+	.toggle-button{
+		text-align: center;
+		display: block;
+		cursor: pointer;
+		color: black;
+		margin-top:45px;
+	}
+	.toggle-button:after{
+		content: attr(data-open);
+		display:block;
+	}
+	#menu-checkbox:checked + nav[role="navigation"] .main-menu{
+		display:block;
+	}
+		#menu-checkbox:checked + nav[role="navigation"] .toggle-button:after{
+			content:attr(data-close);
+		}
+		.header-container a{
+			margin-right:-65px;
+		}
+		.main-menu{
+			position: relative;
+			top: 30px;
+			right: 115px;
+		}
+}*/
