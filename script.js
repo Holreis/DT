941340c150073e09800c9b1608e92682277f43e8
dt_origami <shayazdanov.artem@gmail.com>
2019-03-03 00:35:50 +0300

    Первое задание

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 'use strict';
-let money = +prompt("Ваш бюджет на месяц?", "");
+let money = prompt("Ваш бюджет на месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "");
-let expenses = {};
+
 let appData = {
-    budjet: money,
-    vremy: time,
+    budget: money,
     expenses: {},
     optionalExpenses: {},
-    income: {},
+    income: [],
+    timeData: time,
     savings: false
 };
-let necessaryExpenses = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    cost = prompt("Во сколько обойдется?", "");
-appData.expenses[necessaryExpenses] = cost;
-console.log(appData);
-alert('бюджет на 1 день: ' + appData.budjet/30 + ' рублей');
